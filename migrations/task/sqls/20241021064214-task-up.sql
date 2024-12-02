69afb03a8c68221798b612a8adcb1a3a3962bafb
Wanyu <q37375@gmail.com>
2024-12-03 01:57:12 +0800

    test

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -14,8 +14,7 @@
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
-INSERT INTO "USER"(name, email, role) 
-VALUES
+INSERT INTO "USER"(name, email, role) VALUES
 ('李燕容','lee2000@hexschooltest.io','USER'),
 ('王小明','wXlTq@hexschooltest.io','USER'),
 ('肌肉棒子','muscle@hexschooltest.io','USER'),
